20dac2d7d5e9cc32744241237856ddea992c33aa
everpeznul <mafiramul@pvmpok.ru>
2025-06-15 17:47:42 +0300

    test commit

---
+++ internal/domain/note.go
@@ -0,0 +1,18 @@
+package domain
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Note struct {
+	Id         uuid.UUID
+	Title      string
+	Path       string
+	Class      string
+	Tags       []string
+	Links      []string
+	Content    string
+	CreateTime time.Time
+	UpdateTime time.Time
+}
